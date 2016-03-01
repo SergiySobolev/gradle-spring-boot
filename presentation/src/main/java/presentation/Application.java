176ef6758bc8502d2b0124ccd64969b1ffd5c85a
ssobolev <sergiy.sobolev@gmail.com>
2016-03-01 16:27:00 +0200

    Resolve url to another module

--- presentation/src/main/java/presentation/Application.java
+++ presentation/src/main/java/presentation/Application.java
@@ -4,14 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
 
 }
---
+++ presentation/src/main/java/presentation/config/WebConfig.java
@@ -0,0 +1,58 @@
+package presentation.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.Assert;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${resources.projectroot:}")
+    private String projectRoot;
+
+    @Value("${app.version:}")
+    private String appVersion;
+
+    private String getProjectRootRequired() {
+        Assert.state(this.projectRoot != null, "Please set \"resources.projectRoot\" in application.yml");
+        return this.projectRoot;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        boolean devMode = this.env.acceptsProfiles("dev");
+
+        String location = devMode ? "file:///" + getProjectRootRequired() + "/web/src/" : "classpath:static/";
+        Integer cachePeriod = devMode ? 0 : null;
+        boolean useResourceCache = !devMode;
+        String version = getApplicationVersion();
+
+        AppCacheManifestTransformer appCacheTransformer = new AppCacheManifestTransformer();
+        VersionResourceResolver versionResolver = new VersionResourceResolver()
+                .addFixedVersionStrategy(version, "/**/*.js", "/**/*.map")
+                .addContentVersionStrategy("/**");
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations(location)
+                .setCachePeriod(cachePeriod)
+                .resourceChain(useResourceCache)
+                .addResolver(versionResolver)
+                .addTransformer(appCacheTransformer);
+    }
+
+    protected String getApplicationVersion() {
+        return this.env.acceptsProfiles("dev") ? "dev" : this.appVersion;
+    }
+
+
+}
