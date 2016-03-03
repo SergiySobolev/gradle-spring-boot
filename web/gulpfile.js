5e680f0b0be5d141d41dff579e8a7505b8758873
ssobolev <sergiy.sobolev@gmail.com>
2016-03-03 16:53:13 +0200

    gulp copy bower vendor files

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,16 +1,36 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    gclean = require('gulp-clean'),
+    gFlatten = require('gulp-flatten'),
+    mainBowerFiles = require('main-bower-files')
+;
 
-gulp.task('message', function() {
-    return gutil.log('Gulp is running!')
+var paths = {
+    src: 'app/',
+    dist: 'dist/',
+    vendors_dir: 'bower_components/',
+    vendors:['angular/angular.js', 'angular-resource/angular-resource.js']
+};
+
+gulp.task("bower-files", function(){
+    gulp.src(mainBowerFiles())
+        .pipe(gFlatten())
+        .pipe(gulp.dest("./app/vendors"));
 });
 
-gulp.task('html', function() {
-    return gutil.log('Html changed')
+gulp.task('clean', function(){
+    return gulp.src([paths.dist], {read:false})
+        .pipe(gclean());
 });
 
-gulp.task('build', ['message'], function(){ });
+gulp.task('html', function () {
+    gulp.src(paths.src+'index.html')
+        .pipe(gulp.dest(paths.dist));
+});
 
-gulp.task('watch', function() {
-    gulp.watch('app/src/index.html', ['html']);
+gulp.task('build', ['clean', 'bower-files'], function () {
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.src+'index.html', ['html']);
 });
