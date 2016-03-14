097cbe5112940cf7d21c3b676add003727e195df
ssobolev <sergiy.sobolev@gmail.com>
2016-03-14 16:51:28 +0200

    chart stub

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -8,15 +8,23 @@ var gulp = require('gulp'),
     gRunSequence = require('run-sequence'),
     angularFileSort = require('gulp-angular-filesort'),
     series = require('stream-series'),
+    gLess = require('gulp-less'),
     gPrint = require('gulp-print')
 ;
 
 var paths = {
     any: '**/*',
     src: 'src/',
+    srcLess : 'src/less/',
     dist: 'dist/',
     distVendors: 'dist/vendors',
-    distApp: 'dist/app'
+    distApp: 'dist/app',
+    distCss: 'dist/css'
+};
+
+var files = {
+  angular : '/angular.js',
+  chart: '/Chart.js'
 };
 
 var options = {
@@ -36,7 +44,7 @@ gulp.task('clean', function(){
 
 gulp.task('buildDist', [], function(){
 
-    gulp.src(paths.src+paths.any)
+    gulp.src([paths.src+paths.any, "!"+paths.srcLess+paths.any])
         .pipe(gulp.dest(paths.dist));
 
     return gulp.src(mainBowerFiles())
@@ -44,23 +52,32 @@ gulp.task('buildDist', [], function(){
         .pipe(gulp.dest(paths.distVendors));
 });
 
+gulp.task('lessCompile', [], function(){
+    return gulp.src(paths.srcLess+"*.less")
+        .pipe(gLess())
+        .pipe(gulp.dest(paths.distCss));
+});
+
 gulp.task('injectDist', [], function(){
     var injectOptions = {
         relative:true
     };
     var target = gulp.src(paths.dist+'index.html');
-    var angularSource = gulp.src(paths.distVendors + "/angular.js");
-    var vendorSources = gulp.src([paths.distVendors + "**/*.js", "!dist/vendors/angular.js"], {read: false});
+    var angularSource = gulp.src([paths.distVendors + files.angular,
+                                  paths.distVendors + files.chart]);
+    var vendorSources = gulp.src([paths.distVendors + "**/*.js",
+                                "!" + paths.distVendors + files.angular,
+                                "!" +paths.distVendors + files.chart], {read: false});
     var appSources = gulp.src(paths.distApp + "/**/*.js");
-    vendorSources.pipe(gPrint());
+    var cssSources = gulp.src(paths.dist + "**/*.css");
     return target
         .pipe(gDebug())
-        .pipe(gInject(series(angularSource, vendorSources, appSources.pipe(angularFileSort())), injectOptions))
+        .pipe(gInject(series(angularSource, vendorSources, appSources.pipe(angularFileSort()), cssSources), injectOptions))
         .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('build', function () {
-    gRunSequence('injectDist');
+    gRunSequence('clean', 'buildDist', 'lessCompile', 'injectDist');
 });
 
 gulp.task('watch', function () {
