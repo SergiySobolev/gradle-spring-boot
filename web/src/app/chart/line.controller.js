db0271539e42bdddfb87d35635562f572f1af558
SergiySobolev <1nt3gr@L>
2016-03-15 00:01:13 +0200

    refactored LineController according best practices

--- web/src/app/chart/line.controller.js
+++ web/src/app/chart/line.controller.js
@@ -2,14 +2,17 @@ angular
     .module('app')
     .controller('LineController', LineController);
 
-function LineController($scope){
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
+function LineController(){
+    var vm = this;
+    vm.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    vm.series = ['Series A', 'Series B'];
+    vm.colors = ['Green', 'Dark Grey', 'Grey'];
+    vm.data = [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
     ];
-    $scope.onClick = function (points, evt) {
+    vm.onClick = function (points, evt) {
         console.log(points, evt);
     };
+    return this;
 }
